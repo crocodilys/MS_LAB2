abf585eda841298f5cb1284091340689025ba0de
Dmitry Stabrovsky <dmitry.stabrovsky@gmail.com>
2014-03-05 17:18:12 +0300

    cfg added

--- weather.sh
+++ weather.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+. $PWD/weather.cfg
 while true
 do
 	curl -s http://pogoda.tut.by/city/minsk > minsk
 	grep -m 1 '<span class="temp-i">' minsk > temp
 	echo "Current weather in Minsk: " 	$(sed 's/[^\-\+[0-9]//g' temp)
 	rm -r temp minsk
-	sleep 1s
+	sleep $sleep_time
 done
